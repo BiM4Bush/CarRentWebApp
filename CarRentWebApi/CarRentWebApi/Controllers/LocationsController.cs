c2a402b3b8179f4decf500ac7306d0b973e29d56
BiM4Bush <sebakrzynek@gmail.com>
2024-06-13 18:15:05 +0200

    Created LocationsController with get locations method

---
+++ CarRentWebApi/CarRentWebApi/Controllers/LocationsController.cs
@@ -0,0 +1,23 @@
+﻿using CarRentWebApi.Data;
+using CarRentWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRentWebApi.Controllers
+{
+    public class LocationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LocationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Location>>> GetLocations()
+        {
+            return await _context.Locations.ToListAsync();
+        }
+    }
+}
