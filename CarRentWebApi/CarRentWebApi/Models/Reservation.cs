ab917cadc19f50f2fc358bf09194dfaf406cd60e
BiM4Bush <sebakrzynek@gmail.com>
2024-06-13 17:50:58 +0200

    Set models

---
+++ CarRentWebApi/CarRentWebApi/Models/Reservation.cs
@@ -0,0 +1,17 @@
+﻿namespace CarRentWebApi.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public int PickupLocationId { get; set; }
+        public int ReturnLocationId { get; set; }
+        public DateTime PickupDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public decimal RentCost { get; set; }
+
+        public Car Car { get; set; }
+        public Location PickupLocation { get; set; }
+        public Location ReturnLocation { get; set; }
+    }
+}
