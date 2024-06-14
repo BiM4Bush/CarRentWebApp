3060b96b9b9954272bff42ed1ee71d28fcd2349e
BiM4Bush <sebakrzynek@gmail.com>
2024-06-14 14:51:46 +0200

    Set and created some tests

--- CarRentWebApi/CarRentWebApi/Models/Car.cs
+++ CarRentWebApi/CarRentWebApi/Models/Car.cs
@@ -5,5 +5,12 @@
         public int Id { get; set; }
         public string Model { get; set; }
         public decimal RentCostPerDay { get; set; }
+
+        public Car(int id, string model, decimal rentCostPerDay) //only for test use, may change in the future for random generating Guid Id 
+        {
+            Id = id;
+            Model = model;
+            RentCostPerDay = rentCostPerDay;
+        }
     }
 }
